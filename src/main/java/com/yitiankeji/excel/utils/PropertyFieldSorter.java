7425454df7f023f56b284cdd5946e59938e8c335
tianlihu <tianlihu@qq.com>
2023-01-18 17:20:32 +0800

    解决index=-1，无value值问题

--- src/main/java/com/yitiankeji/excel/utils/PropertyFieldSorter.java
+++ src/main/java/com/yitiankeji/excel/utils/PropertyFieldSorter.java
@@ -1,6 +1,7 @@
 package com.yitiankeji.excel.utils;
 
 import com.yitiankeji.excel.annotation.ExcelProperty;
+import lombok.Data;
 
 import java.lang.reflect.Field;
 import java.util.*;
@@ -9,13 +10,7 @@ public class PropertyFieldSorter {
 
     public static List<Field> getIndexFields(Class<?> type) {
         List<Field> fields = getAllFields(type);
-        Map<Integer, Field> indexFieldMap = new TreeMap<>();
-        List<Field> unindexFields = new ArrayList<>(fields.size());
-        List<Field> propertyFields = new ArrayList<>(fields.size());
-
-        cacheFields(type, fields, indexFieldMap, unindexFields, propertyFields);
-
-        return new ArrayList<>(indexFieldMap.values());
+        return sortedFields(fields);
     }
 
     private static List<Field> getAllFields(Class<?> type) {
@@ -26,24 +21,63 @@ public class PropertyFieldSorter {
         return fields;
     }
 
-    private static void cacheFields(Class<?> type, List<Field> fields, Map<Integer, Field> fieldMap, List<Field> unindexFields, List<Field> propertyFields) {
+    private static List<Field> sortedFields(List<Field> fields) {
+        Map<Integer, Entry> indexFieldMap = new TreeMap<>();
+        List<Field> unindexFields = new ArrayList<>(fields.size());
+
+        int maxIndex = -1;
         for (Field field : fields) {
             ExcelProperty property = field.getAnnotation(ExcelProperty.class);
             if (property == null) {
                 continue;
             }
             int index = property.index();
-            if (index != -1) {
-                Field cachedField = fieldMap.get(index);
-                if (cachedField != null) {
-                    throw new RuntimeException("Field property index ( " + index + " ) is already exists in Class " + type.getCanonicalName());
-                }
+            if (index > maxIndex) {
+                maxIndex = index;
             }
-            fieldMap.put(index, field);
-            if (index < 0) {
+
+            if (index != -1) {
+                Entry entry = indexFieldMap.get(index);
+                if (entry == null) {
+                    entry = new Entry(index);
+                    indexFieldMap.put(index, entry);
+                }
+                entry.getFields().add(field);
+            } else {
                 unindexFields.add(field);
             }
-            propertyFields.add(field);
+        }
+
+        Iterator<Field> unindexFieldIterator = unindexFields.iterator();
+        List<Field> propertyFields = new ArrayList<>(fields.size());
+        for (int i = 0; i <= Math.max(fields.size(), maxIndex); i++) {
+            Entry entry = indexFieldMap.get(i);
+            if (entry != null) {
+                propertyFields.addAll(entry.getFields());
+                continue;
+            }
+            boolean has = unindexFieldIterator.hasNext();
+            if (!has) {
+                continue;
+            }
+            propertyFields.add(unindexFieldIterator.next());
+        }
+
+        return propertyFields;
+    }
+
+    @Data
+    static class Entry implements Comparable<Entry> {
+        private int index;
+        private List<Field> fields = new ArrayList<>();
+
+        public Entry(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Entry o) {
+            return index - o.index;
         }
     }
 }
